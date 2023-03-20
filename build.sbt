9b6cb7d26bd17c8b715604dffff59d25b77912c2
Luis Vegas <luisvegasmor@gmail.com>
2023-03-19 20:51:25 -0400

    feat: basic fields extraction

--- build.sbt
+++ build.sbt
@@ -1,13 +1,14 @@
-val scala3Version = "3.2.2"
+val scala3Version = "3.1.3"
 
 lazy val root = project
   .in(file("."))
   .settings(
     name := "evidence",
     version := "0.1.0-SNAPSHOT",
-
     scalaVersion := scala3Version,
-
-    libraryDependencies ++= Seq("org.scalameta" %% "munit" % "0.7.29" % Test,
-    "com.github.tototoshi" %% "scala-csv" % "1.3.10")
+    libraryDependencies ++= Seq(
+      "org.scalameta" %% "munit" % "0.7.29" % Test,
+      "com.github.tototoshi" %% "scala-csv" % "1.3.10",
+      "com.github.losizm" %% "t2" % "2.0.0"
+    )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,45 @@
 //> using scala "3"
 //> using lib "com.github.tototoshi::scala-csv:1.3.10"
+//> using lib "com.github.losizm::t2:2.0.0"
+
 import com.github.tototoshi.csv._
 import scala.io.Source
 
-@main def hello: Unit =
-  val reader = CSVReader.open(Source.fromFile("src/main/resources/data.csv"))
-  println( reader.readNext() )
+@main def main: Unit =
+  def desugarReader(el: Seq[String]): (String, String, String) = {
+    println(el)
+    println("Hola")
+    val id = el(0)
+    val severity = el(9)
+    val desc = el(10)
 
-def msg = "I was compiled by Scala 3. :)"
+    (id, severity, desc)
+  }
+
+  def evidencePrinter(id: String, sev: String, desc: String): Unit = {
+    val table = t2
+      .TableBuilder()
+      .add("ID", "Severity", "Description")
+      .add(id, sev, desc)
+      .build()
+
+    val writer = t2.TableWriter(
+      "ansiColorEnabled" -> "true",
+      "tableBorderColor" -> "cyan",
+      "tableHeaderColor" -> "black,greenBackground",
+      "bodyRuleColor" -> "green",
+      "rowHeaderEnabled" -> "true",
+      "rowHeaderColor" -> "bold,cyan",
+      "columnRightAlign" -> "0,5" // Right align first and last columns
+    )
+
+    writer.write(System.out, table)
+
+  }
+  val reader = CSVReader
+    .open(Source.fromFile("src/main/resources/data.csv"))
+    .iterator
+    .drop(1)
+
+  val foo = reader.take(3).foreach(desugarReader)
+  // evidencePrinter(els._1, els._2, els._3)
