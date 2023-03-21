64384a965dc35ecb4fcfe43936c568c3fd530a7c
Luis Vegas <luisvegasmor@gmail.com>
2023-03-20 22:01:57 -0400

    feat: evidences are wrapped in a small case class. Tables are printing

---
+++ src/main/scala/Evidence.scala
@@ -0,0 +1,1 @@
+final case class Evidence(id: String, sev: String, description: String)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,45 +1,49 @@
 //> using scala "3"
 //> using lib "com.github.tototoshi::scala-csv:1.3.10"
 //> using lib "com.github.losizm::t2:2.0.0"
-
 import com.github.tototoshi.csv._
 import scala.io.Source
+import scala.collection.mutable.ListBuffer
 
 @main def main: Unit =
-  def desugarReader(el: Seq[String]): (String, String, String) = {
-    println(el)
-    println("Hola")
-    val id = el(0)
-    val severity = el(9)
-    val desc = el(10)
-
-    (id, severity, desc)
-  }
-
-  def evidencePrinter(id: String, sev: String, desc: String): Unit = {
-    val table = t2
+  var evidences: ListBuffer[Evidence] = new ListBuffer[Evidence]()
+  def evidencePrinter(evs: ListBuffer[Evidence]): Unit = {
+    var table: t2.TableBuilder = t2
       .TableBuilder()
       .add("ID", "Severity", "Description")
-      .add(id, sev, desc)
-      .build()
+    evs.foreach(e => table.add(e._1, e._2, e._3))
 
-    val writer = t2.TableWriter(
+    var writer = t2.TableWriter(
       "ansiColorEnabled" -> "true",
       "tableBorderColor" -> "cyan",
       "tableHeaderColor" -> "black,greenBackground",
       "bodyRuleColor" -> "green",
       "rowHeaderEnabled" -> "true",
       "rowHeaderColor" -> "bold,cyan",
-      "columnRightAlign" -> "0,5" // Right align first and last columns
+      "cellPadSize" -> "1",
+      "cellSpaceSize" -> "1",
+      "maxValueSize" -> "100"
     )
 
-    writer.write(System.out, table)
-
+    writer.write(System.out, table.build())
   }
+
+  def desugarToEvidence(el: Seq[String]): Evidence = {
+    val id = el(0)
+    val severity = el(9)
+    val desc = el(10)
+
+    val ev = Evidence(id, severity, desc)
+
+    evidences += ev
+    ev
+  }
+
+  // Drop the header for now
   val reader = CSVReader
     .open(Source.fromFile("src/main/resources/data.csv"))
     .iterator
     .drop(1)
 
-  val foo = reader.take(3).foreach(desugarReader)
-  // evidencePrinter(els._1, els._2, els._3)
+  val foo = reader.take(9).foreach(desugarToEvidence)
+  evidencePrinter(evidences)
